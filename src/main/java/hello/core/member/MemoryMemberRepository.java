30d09f4c1e4b458c2a88be64288cc4af15b3595d
yuio7279 <yuio7279@gmail.com>
2023-10-24 11:45:15 +0900

    회원 도메인 개발

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    //참고: HashMap 은 동시성 이슈가 발생할 수 있다. 이런 경우 ConcurrentHashMap 을 사용하자.
+    private static Map<Long, Member> store = new HashMap<>();
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
