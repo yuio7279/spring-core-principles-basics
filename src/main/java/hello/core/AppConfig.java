40d6f4ee0b75883cc405fdb8bb7189b50143907b
yuio7279 <yuio7279@gmail.com>
2023-10-28 18:44:20 +0900

    스프링으로 전환하기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,23 +9,34 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+//스프링으로 전환하기 - 어노테이션 사용
+//Configuration 구성정보를 담당 한다는 의미
+@Configuration
 public class AppConfig {
 
     //메서드 명만 보았을 때 역할, 즉 주입관계를 한 눈에 볼 수 있다.
+
+    //스프링 컨테이너에 Bean으로써 등록 됨
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
-
+    @Bean
     public OrderService ordersErvice(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     //설정정보를 한 눈에 들어오게 하기 위해 새로운 메서드로 정의하였다.
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         //return new FixDiscountPolicy();
         //정책변경
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,11 +4,23 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+/*        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();*/
+
+        //스프링으로 전환하기 - Bean들을 관리하는 ApplicationContext
+        //AnnotationConfigApplicationContext - 어노테이션으로 선언한 Bean을 관리
+        //그냥 객체를 생성해서 넣는것과 AppConfig.class 이렇게 해서 넣는 것의 차이?
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //name = AppConfig안의 메서드명(객체 명)
+        //두번째는 리턴 할 반환 타입
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
 
         Member member = new Member(1L, "memberA", Grade.VIP);
 
