07179f1e5cdb70b3c43d9dcee53cc9649133ac53
yuio7279 <yuio7279@gmail.com>
2023-10-28 18:08:36 +0900

    AppConfig 리팩터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -10,11 +10,24 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
+
+    //메서드 명만 보았을 때 역할, 즉 주입관계를 한 눈에 볼 수 있다.
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
+
     public OrderService ordersErvice(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
+    //설정정보를 한 눈에 들어오게 하기 위해 새로운 메서드로 정의하였다.
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
+
 
 }
