fe7fc5ee34126ec0df321fd8ad38c962f2e79980
yuio7279 <yuio7279@gmail.com>
2023-10-28 18:55:25 +0900

    스프링으로 전환하기

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -18,6 +18,7 @@ public class OrderApp {
         OrderService orderService = appConfig.ordersErvice();*/
 
         //스프링으로 전환하기
+        //ApplicationContext는 스프링 컨테이너라고 할 수 있다.
         ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
         OrderService orderService = ac.getBean("orderService", OrderService.class);
         MemberService memberService = ac.getBean("memberService", MemberService.class);
