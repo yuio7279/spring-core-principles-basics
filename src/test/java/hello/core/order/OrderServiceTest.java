c53eb4b7e46e7113969dae76b49ae29ef34af648
yuio7279 <yuio7279@gmail.com>
2023-10-28 17:16:35 +0900

    주문과 할인 도메인 실행과 테스트

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,25 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder(){
+        //기초타입으로 할 때는 null을 넣을 수가 없다 하지만 여기에선 사용해도 무방
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order itemA = orderService.createOrder(memberId, "itemA", 10000);
+
+        Assertions.assertThat(itemA.getDiscountPrice()).isEqualTo(1000);
+    }
+}
