4f48c24cbce510112ac2ad603e2f95531e84c062
yuio7279 <yuio7279@gmail.com>
2023-11-03 18:29:52 +0900

    조회한 빈이 모두 필요할 때 - List, Map

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 //컴포넌트 스캔용 설정정보파일
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core.member",
         excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
