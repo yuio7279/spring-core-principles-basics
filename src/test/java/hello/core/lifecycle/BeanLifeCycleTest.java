b14f5a150d354444a6e59b821effc86be7e4bef6
yuio7279 <yuio7279@gmail.com>
2023-11-11 13:35:06 +0900

    빈 생명주기 인터페이스 InitializingBean DisposableBean

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -14,9 +14,10 @@ public class BeanLifeCycleTest {
         NetworkClient client = ac.getBean("networkClient", NetworkClient.class);
 
         /**
-         * ApplicationContext를 (사용을 안해서?) 닫아야한다.
+         * ApplicationContext를 (빈들의 생명주기를 확인하기 위해) 닫아야한다.
          * 하지만 기본적으로 제공해주지 않는다. 따라서 구체인 AnnotationConfigApplicationContext 로 바꾸거나
          * ConfigurableApplicationContext를 사용한다.
+         * 스프링 컨테이너를 종료한 것
          */
         ac.close();
     }
--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,12 +1,13 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
 
     public NetworkClient(){
         System.out.println("생성자호출, url = "+url);
-        connect();
-        call("초기화 연결 메시지");
     }
     public void setUrl(String url){
         this.url = url;
@@ -24,5 +25,19 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect(){
         System.out.println("close "+ url);
+    }
+
+
+    //프로퍼티셋(의존관계 주입)이 끝난 후에 호출
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    //스프링 빈이 종료될 때 호출
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
     }
 }
