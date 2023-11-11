3b4d75e7afc5f0aae4916c0bbbcf8787f02110e0
yuio7279 <yuio7279@gmail.com>
2023-11-11 14:29:46 +0900

    어노테이션 PostConstruct, PreDestroy

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,7 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 public class NetworkClient {
     private String url;
 
@@ -25,12 +27,13 @@ public class NetworkClient {
     }
 
 
-
+    @PostConstruct
     public void init(){
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
+    @PreDestroy
     public void close(){
         System.out.println("NetworkClient.close");
         disconnect();
